54bc43e995770536390e203a57142f905eae7f78
Batiste1998 <batiste.blactot1@gmail.com>
2024-11-15 01:34:34 +0100

    add cors and 404 not found

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const pokemonRouter = require('./routes/pokemon')
 require('dotenv').config()
 const app = express()
@@ -12,9 +13,14 @@ mongoose
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error(err))
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use('/api/pokemons', pokemonRouter)
+
+app.use((req, res) => {
+  res.status(404).send("Sorry can't find that route.")
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
